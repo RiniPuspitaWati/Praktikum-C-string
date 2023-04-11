36215778a64db97af8d53badd8cc57a52a52a5dc
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-11 22:47:30 +0700

    Add files via upload

---
+++ Praktikum C-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char nama[15]="Rini";
+	char prodi[]="Ilkom";
+	
+	cout<<nama<<endl;
+	cout<<sizeof(nama)<<endl;
+	
+	cout<<prodi<<endl;
+	cout<<sizeof(prodi)<<endl;
+}
